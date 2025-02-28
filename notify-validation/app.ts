6696f7143d95856e421ea0722a5595498483b7b8
igorcruzz <igorcruz.dev@gmail.com>
2025-02-28 20:28:12 -0300

    refactor: add validation fields

--- notify-validation/app.ts
+++ notify-validation/app.ts
@@ -1,8 +1,23 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
+type NotifyType = {
+    userId: string;
+    message: string;
+    priority: 'HIGH' | 'MEDIUM' | 'LOW';
+};
+
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
-        const body = JSON.parse(event.body || '');
+        const body: NotifyType = JSON.parse(event.body || '');
+
+        if (!body.userId || !body.message || !body.priority) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'Parâmetros inválidos',
+                }),
+            };
+        }
 
         return {
             statusCode: 200,
