3f4d54c59e9a9402542c71ed2b8037ba11e2b4b5
igorcruzz <igorcruz.dev@gmail.com>
2025-03-07 23:29:45 -0300

    feat: add put item on dynamodb

--- notify-process/app.ts
+++ notify-process/app.ts
@@ -1,7 +1,32 @@
 import { SQSEvent } from 'aws-lambda';
+import { DynamoDBClient, PutItemCommand, PutItemCommandInput } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async (event: SQSEvent): Promise<void> => {
     for (const record of event.Records) {
-        console.log('Processing record: ', record);
+        try {
+            const body = JSON.parse(record.body);
+
+            console.log('Processing message', body);
+
+            const parmas: PutItemCommandInput = {
+                TableName: process.env.TABLE_NAME,
+                Item: {
+                    ID: body.userId,
+                    Message: body.message,
+                    Priority: body.priority,
+                },
+                ConditionExpression: 'attribute_not_exists(ID)',
+            };
+
+            const command = new PutItemCommand(parmas);
+
+            await docClient.send(command);
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
