08fa37b1a3540511e7d6d0384d2f98b01a3be07d
igorcruzz <igorcruz.dev@gmail.com>
2025-03-13 20:27:26 -0300

    feat: create process dlq function

---
+++ notify-process-dlq/app.ts
@@ -0,0 +1,12 @@
+import { SQSEvent } from 'aws-lambda';  
+
+export const handler = async (event: SQSEvent): Promise<void> => {
+    for (const record of event.Records) {
+        try {
+            const body = JSON.parse(record.body); 
+            console.log(body);
+        } catch (error) {
+            console.log(error); 
+        }
+    }
+};
